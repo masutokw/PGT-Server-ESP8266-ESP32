92b17c3eacd6f8886103d7024ee076ecf166b8fa
masutokw <masutokw@users.noreply.github.com>
2016-10-28 14:19:22 +0200

    Support for  SSD1306 OLED Display.

--- esp8266pgt.ino
+++ esp8266pgt.ino
@@ -2,6 +2,8 @@
 #include <time.h>
 #include "sntp.h"
 #include <Ticker.h>
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306.h"
 #include "misc.h"
 #include "piclevel.h"
 #include "mount.h"
@@ -19,7 +21,8 @@ extern char  response[200];
 mount_t *mount_test;
 Ticker speed_control_tckr,counters_poll_tkr;
 extern long command( char *str );
-
+time_t now;
+SSD1306  display(0x3c, D5, D6);
 int net_task(void)
 {
     int lag=0;
@@ -35,8 +38,8 @@ int net_task(void)
             {
                 if (serverClients[i]) serverClients[i].stop();
                 serverClients[i] = server.available();
-                Serial1.println("New client: ");
-                Serial1.println(i);
+             //   Serial1.println("New client: ");
+             //   Serial1.println(i);
                 continue;
             }
         }
@@ -55,14 +58,18 @@ int net_task(void)
 
                 while (serverClients[i].available())
                 {
-                    delay(10);
+                    delay(10); display.clear();
                     size_t n = serverClients[i].available();
                     serverClients[i].readBytes(buff, n);
                     command( buff);
-                    Serial.write(buff,n);
-                    Serial.println();
+                   // Serial.write(buff,n);
+                   buff[n]=0;
+                    display.drawString(0,0, String(buff)+" "+String(response));
+                   // Serial.println();
+                     display.drawString(0,15,ctime(&now));
                     serverClients[i].write((char*)response,strlen(response));
-                    Serial.write(response,strlen(response));
+                   // Serial.write(response,strlen(response));
+                    display.display();
                     // checkfsm();
                 }
 
@@ -74,45 +81,57 @@ int net_task(void)
 
 
 void setup()
-{
-    Serial1.begin(115200);
-    Serial1.print("\nConnecting to ");
-    Serial1.println(ssid);
+{  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.clear();
+  display.drawString(0, 0, "Connecting to "+String(ssid));
+  display.display();
+    
+
     WiFi.begin(ssid, password);
     delay(500);
     uint8_t i = 0;
     while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
     if(i == 21)
     {
-        Serial1.print("Could not connect to");
-        Serial1.println(ssid);
+       
         while(1) delay(500);
     }
+     display.clear();
+     display.drawString(0, 0, "Connecting to "+String(ssid));
+     display.drawString(0, 13, "Got IP! :"+String(WiFi.localIP()));
+     display.drawString(0, 26, "Waiting for Client");
+     display.display();
     configt();
 //start UART and the server
     Serial.begin(BAUDRATE);
     server.begin();
     server.setNoDelay(true);
-
     mount_test=create_mount();
     mount_test->azmotor->speed=0.0;
     mount_test->altmotor->speed=0.0;
     mount_test->azmotor->targetspeed=1;//5.042*SEC_TO_RAD;
     mount_test->altmotor->targetspeed=1;//5.042*SEC_TO_RAD;
 
-//sync_ra_dec(mount_test);
-//set_mount_target( mount_test,1.4 ,4);
-//destroy_mount(mount_test);
+
     speed_control_tckr.attach_ms(100,thread_motor,mount_test);
-    // counters_poll_tkr.attach_ms(300,thread_counter,mount_test);
+    counters_poll_tkr.attach_ms(300,thread_counter,mount_test);
 }
 
 void loop()
-{
+{ 
+ // display.drawString(0, 0, String(millis()));
+  // write the buffer to the display
+ // display.display();
+ 
 
 // motor_set_period (0x30, 0x3132, 0x34); delay(1000);
 // Serial.println();
     delay(300-net_task());
+    //delay(10);
+    
 //mount_test->azmotor->targetspeed+=(1.0)*SEC_TO_RAD;
 //mount_test->altmotor->targetspeed-=(0.1)*SEC_TO_RAD;
 //Serial.print(mount_test->altmotor->speed,6);
@@ -123,10 +142,10 @@ void loop()
 //pollcounters(254);
 //    Serial.println(mount_test->azmotor->counter);
     //Serial.println(Serial.available());
-    Serial.println(sidereal_timeGMT(-4.0),7 );
-    time_t now = time(nullptr);
-    Serial.println(ctime(&now));
-    Serial.println(now);
+   // Serial.println(sidereal_timeGMT(-4.0),7 );
+  now   = time(nullptr);
+  //  Serial.println(ctime(&now));
+
 
 
 }
